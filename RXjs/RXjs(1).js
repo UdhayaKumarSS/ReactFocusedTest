00b85bc978ea3d979d7ba9821ff1f992cbb5bb8d
uday <udhayakumar@softsuave.com>
2020-03-11 20:09:27 +0530

    modified the RXjs answers

--- RXjs/RXjs(1).js
+++ RXjs/RXjs(1).js
@@ -1,11 +1,14 @@
-import { Subject } from "rxjs";
-const stream = [
-  [1, 2],
-  [3, 4],
-  [5, 6, 7]
-];
-const sub = new Subject();
-sub.subscribe(i => i.forEach(v => console.log(v)));
-stream.forEach(input => {
-  sub.next(input);
-});
+// 2) If you have an array of values in a stream and you wish to pipe it such that it will emit the arrays values individually, one by one and wait for them all to be completed before processing another array, how would you do this?  Please provide a code example.
+// E.g.
+// [1,2] -> [3,4] -> [5,6,7]
+// Should produce:
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+
+// Answer : 
+
+import { from, concat } from 'rxjs';
+
+const mergedLastestArray = concat([1,2],[3,4],[5,6,7])
+const arraySource = from(mergedLastestArray);
+const subscribe = arraySource.subscribe(val => console.log(val));
+
--- RXjs/RXjs(2).js
+++ RXjs/RXjs(2).js
@@ -1,13 +1,36 @@
-import { Subject } from "rxjs";
+// 3) If you have a stream that receives individual values and would like to pipe it such that it builds an array out of these values, emitting the updated array each time a new value is added to it, how would you do this?  Please provide a code example.
+// E.g.
+// 1 -> 2 -> 3 -> 4
+// Should produce:
+// [1] -> [1,2] -> [1,2,3] -> [1,2,3,4]
 
-const stream = [1, 2, 3, 4];
-const output = [];
-const sub = new Subject();
-sub.subscribe(i => {
-  output.push(i);
-  console.log(output);
-  return output;
+// Answer : 
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { from } from 'rxjs';
+
+const numbers = from([1,2,3,4]);
+let someArray = [];
+const finalArray= numbers.pipe(take(4));
+ 
+ 
+finalArray.subscribe(x =>{
+  someArray.push(x);
+console.log(someArray)
 });
-stream.forEach(input => {
-  sub.next(input);
+
+
+// Alternative Way
+
+import { interval, from } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+const numbers = interval(1000);
+let someArray = [];
+const finalArray= numbers.pipe(take(4));
+ 
+ 
+finalArray.subscribe(x =>{
+  someArray.push(x + 1);
+console.log(someArray)
 });
